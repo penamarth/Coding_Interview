c5a90d3cb8d5d12acc67ff5e3845ba18b20be264
Пётр Поспелов <penamarth@gmail.com>
2025-04-03 13:17:43 +0300

    Решение на C#

---
+++ 1/1.1/C#/prog/Program.cs
@@ -0,0 +1,12 @@
+﻿bool isCharsOccurOnce(string str){
+    foreach( char c in str){
+        if (str.Count(x => x == c) > 1)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+Console.WriteLine(isCharsOccurOnce("abc"));
+Console.WriteLine(isCharsOccurOnce("aabbcc"));
