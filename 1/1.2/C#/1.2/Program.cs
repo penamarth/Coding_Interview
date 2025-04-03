498a5c2b6a441de3ff30fec0322d42a091ea3d06
Пётр Поспелов <penamarth@gmail.com>
2025-04-03 16:13:30 +0300

    Ренешие C#

---
+++ 1/1.2/C#/1.2/Program.cs
@@ -0,0 +1,17 @@
+﻿bool isAnagram(String string1, String string2)
+{
+    if (string1.Length != string2.Length)
+    {
+        return false;
+    }
+
+    foreach (char c in string1)
+    {
+        if (string1.Count(x => x == c) != string2.Count(x => x == c))
+            return false;
+    }
+    return true;
+}
+
+Console.WriteLine(isAnagram("hello", "hell"));
+Console.WriteLine(isAnagram("hello", "ehllo"));
