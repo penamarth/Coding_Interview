cec1476a1badb052cc781f459badf60e527126a9
Пётр Поспелов <penamarth@gmail.com>
2025-04-03 18:09:31 +0300

    Решение C++

---
+++ 1/1.2/С++/1.2/1.2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <algorithm>
+
+bool isAnagram(std::string string1, std::string string2)
+{
+    if (string1.length() != string2.length())
+    {
+        return false;
+    }
+
+    for(char c : string1)
+    {
+        if (std::ranges::count(string1, c) != std::ranges::count(string2, c))
+            return false;
+    }
+    return true;
+}
+
+int main()
+{
+    std::cout << std::boolalpha << isAnagram("hello", "hell") << std::endl;
+    std::cout << std::boolalpha << isAnagram("hello", "ehllo") << std::endl;
+}
