ba77a2bde9fc02460ec9be4c2bf48a9ef8ccd8e0
Пётр Поспелов <penamarth@gmail.com>
2025-04-03 14:11:24 +0300

    Решение на С++

---
+++ 1/1.1/C++/1.1/1.1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <format>
+
+bool isCharsOccurOnce(std::string str) {
+    for(char c : str) {
+        if (std::ranges::count(str, c) > 1)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    std::cout << std::boolalpha << isCharsOccurOnce("abc") << std::endl;
+    std::cout << std::boolalpha << isCharsOccurOnce("aabbcc") << std::endl;
+}
+
