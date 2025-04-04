8696c32ec231d75fa4921dbba4e26cf6e9abb3c8
Пётр Поспелов <penamarth@gmail.com>
2025-04-04 15:04:38 +0300

    Решение C++

---
+++ 1/1.3/С++/1.3/1.3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <regex>
+
+std::string URIWhitespace(std::string s)
+{
+    s = std::regex_replace(s, std::regex("\s+$"), "");
+    s = std::regex_replace(s, std::regex("^\s+"), "");
+    s = std::regex_replace(s, std::regex(" "), "%20");
+    return s;
+}
+
+int main()
+{
+    std::cout << URIWhitespace("NoSpace") << std::endl;
+    std::cout << URIWhitespace("With Space") << std::endl;
+    std::cout << URIWhitespace("Many Spaces In Line") << std::endl;
+    std::cout << URIWhitespace(" StartWithSpace") << std::endl;
+    std::cout << URIWhitespace("EndWithSpace ") << std::endl;
+    std::cout << URIWhitespace(" Start With Space And Multiple Spaces") << std::endl;
+    std::cout << URIWhitespace("End With Space And Multiple Spaces ") << std::endl;
+    std::cout << URIWhitespace(" All In Spaces ") << std::endl;
+}
