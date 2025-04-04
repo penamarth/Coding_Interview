e569480f76cf6b3ba2b5904e5a5f746e38194162
Пётр Поспелов <penamarth@gmail.com>
2025-04-04 11:38:15 +0300

    Решение на C#

---
+++ 1/1.3/C#/1.3/Program.cs
@@ -0,0 +1,13 @@
+﻿String URIWhitespace(String s)
+{
+    return s.Trim().Replace(" ", "%20");
+}
+
+Console.WriteLine(URIWhitespace("NoSpace"));
+Console.WriteLine(URIWhitespace("With Space"));
+Console.WriteLine(URIWhitespace("Many Spaces In Line"));
+Console.WriteLine(URIWhitespace(" StartWithSpace"));
+Console.WriteLine(URIWhitespace("EndWithSpace "));
+Console.WriteLine(URIWhitespace(" Start With Space And Multiple Spaces"));
+Console.WriteLine(URIWhitespace("End With Space And Multiple Spaces "));
+Console.WriteLine(URIWhitespace(" All In Spaces "));
