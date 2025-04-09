d919d139e2c661e5fde0d33e38f5b2639ad63d42
Пётр Поспелов <penamarth@gmail.com>
2025-04-09 09:18:17 +0300

    Решение на C#

---
+++ 1/1.5/C#/1.5/Program.cs
@@ -0,0 +1,48 @@
+﻿bool areOneStepAway(string s1, string s2)
+{
+    if (s1 == s2)
+        return true;
+
+    if (s1.Length == s2.Length)
+    {
+        int count = 0;
+
+        for (int i = 0; i < s1.Length; i++)
+        {
+            if (s1[i] != s2[i])
+            {
+                count += 1;
+            }
+        }
+        if (count == 1)
+            return true;
+        else
+            return false;
+    }
+    else if (s1.Length - s2.Length == 1)
+    {
+        foreach (char c in s2)
+        {
+            if (!s1.Contains(c))
+                return false;
+        }
+        return true;
+    }
+    else if (s2.Length - s1.Length == 1)
+    {
+        foreach (char c in s1)
+        {
+            if (!s2.Contains(c))
+                return false;
+        }
+        return true;
+    }
+    return false;
+}
+
+Console.WriteLine(areOneStepAway("hello", "hello"));
+Console.WriteLine(areOneStepAway("hello", "hell"));
+Console.WriteLine(areOneStepAway("hello", "help"));
+Console.WriteLine(areOneStepAway("hellu", "hello"));
+Console.WriteLine(areOneStepAway("helloo", "hello"));
+Console.WriteLine(areOneStepAway("hellooo", "hello"));
