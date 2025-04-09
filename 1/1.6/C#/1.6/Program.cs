05804c9e9f66a85cdd0fbd157527d46686ec46e5
Пётр Поспелов <penamarth@gmail.com>
2025-04-09 12:28:54 +0300

    Решение на C#

---
+++ 1/1.6/C#/1.6/Program.cs
@@ -0,0 +1,32 @@
+﻿String compressString(String s){
+    String compressed = "";
+    int counter = 1;
+    String prev_c = "";
+    foreach( char c in s) {
+        if (c.ToString() == prev_c)
+            counter += 1;
+        else {
+            compressed = compressed + prev_c + counter.ToString();
+            counter = 1;
+            prev_c = "" + c;
+         }
+    }
+    compressed = compressed + s[s.Length - 1] + counter.ToString();
+    compressed = compressed.Substring(1);
+
+    if(compressed.Length >= s.Length)
+        return s;
+    else
+        return compressed;
+}
+
+
+Console.WriteLine(compressString("abc"));
+Console.WriteLine(compressString("aabc"));
+Console.WriteLine(compressString("Aabc"));
+Console.WriteLine(compressString("aaaabbbcc"));
+Console.WriteLine(compressString("aaAabbBcc"));
+Console.WriteLine(compressString("aaAabbbBcc"));
+Console.WriteLine(compressString("aaAabbbbBcc"));
+Console.WriteLine(compressString("aaAabbbbbBcc"));
+Console.WriteLine(compressString("aaAabbbbbbBcc"));
