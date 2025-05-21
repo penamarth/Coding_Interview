731b6e28fe931738a8a9b2514f5e97e6e9a22a99
Пётр Поспелов <penamarth@gmail.com>
2025-05-21 14:08:57 +0300

    Решение на C#

---
+++ 1/1.9/C#/1.9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+bool isSubstring(String s, String substring)
+{
+    return s.Contains(substring);
+}
+
+bool isCiclicShift(String s1, String s2)
+{
+    if (s1.Length != s2.Length)
+    {
+        return false;
+    }
+    return isSubstring(s2 + s2, s1);
+}
+
+Console.WriteLine("waterbottle " + "erbottlewat " + isCiclicShift("waterbottle", "erbottlewat").ToString());
+Console.WriteLine("Hello " + "oHell " + isCiclicShift("Hello", "oHell").ToString());
+Console.WriteLine("Hello " + "Helloo " + isCiclicShift("Hello", "Helloo").ToString());
+Console.WriteLine("Hello " + "Hoell " + isCiclicShift("Hello", "Hoell").ToString());
